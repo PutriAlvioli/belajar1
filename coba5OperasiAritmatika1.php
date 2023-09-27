f4cdf20085574e1e727047f900104d566c7f4642
PutriAlvioli · she/her <putrialvioliputri@gmail.com>
2023-09-27 22:09:58 +0700

    first commit

---
+++ coba5OperasiAritmatika1.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laprak1</title>
+</head>
+<body>
+    <?
+        $a=10;
+        $b=3;
+        echo "\$a = $a <BR>";
+        echo "\$a = $b <BR>";
+        echo '$a + $b = ';
+        print $a + $b;
+        echo "<BR>";
+        echo '$a - $b = ';
+        print $a - $b;
+        echo "<BR>";
+        echo '$a * $b = ';
+        print $a * $b;
+        echo "<BR>";
+        echo '$a / $b = ';
+        print $a / $b;
+        echo "<BR>";
+        echo '$a % $b = ';
+        print $a % $b;
+    ?>
+</body>
+</html>
